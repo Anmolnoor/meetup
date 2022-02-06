50ebb85469bf37dfed1d40685e3feb74f8139678
Anmolnoor <anmolnoor59@gmaail.com>
2022-02-06 23:28:35 +0530

    New-MeetupForm and Firebase Connected

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import NewMeetupform from "../components/meetups/NewMeetupform";
 
 const New = () => {
-	return <div>this is the new page to get the latest meetup</div>;
+	const navigate = useNavigate();
+	const addMeetupHandler = (meetupData) => {
+		fetch("https://meetups-7ef17-default-rtdb.firebaseio.com/meetups.json", {
+			method: "POST",
+			body: JSON.stringify(meetupData),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}).then(navigate("/", { replace: true }));
+	};
+	return (
+		<section>
+			<h1>Add New Meetups</h1>
+			<NewMeetupform onAddMeetups={addMeetupHandler} />
+		</section>
+	);
 };
 
 export default New;
